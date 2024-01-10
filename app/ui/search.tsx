051ef330566fd65fd44acc48a7ac6466853f9390
Gilberto <66382832+gilbertho502@users.noreply.github.com>
2024-01-09 18:20:02 -0600

    add query params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,16 +5,18 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const {replace} = useRouter()
 
   const handleSearch = (term: string) =>{
     const params = new URLSearchParams(searchParams)
     if (term){
-      params.set('search',term)
+      params.set('query',term)
     }else{
-      params.delete('search')
+      params.delete('query')
     }
 
-    console.log(params.toString())
+    replace(`${pathname}?${params.toString()}`)
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -25,6 +27,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       onChange={(event)=> handleSearch(event.target.value)}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
